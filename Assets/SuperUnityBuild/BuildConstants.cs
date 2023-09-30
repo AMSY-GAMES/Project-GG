91fe09741bbdd55c3dae69bc613eb45a7b0b9a0d
Andrew Shen <azshen23@gmail.com>
2023-09-30 19:29:57 -0400

    build scripts

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,39 @@
+using System;
+
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        ProjectGG,
+    }
+
+    public enum Platform
+    {
+        None,
+        Android,
+        iOS,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Android,
+        iOS,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly DateTime buildDate = new DateTime(638316983510349800);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.ProjectGG;
+    public const Platform platform = Platform.iOS;
+    public const Architecture architecture = Architecture.iOS;
+    public const Distribution distribution = Distribution.None;
+}
+
